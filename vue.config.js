e7245305f7fed6978f68eede9f0beb73d863d488
Subha Chanda <subhachanda88@gmail.com>
2021-05-06 23:32:41 +0530

    removed proxy setup

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://ncov-node-api.herokuapp.com',
-      },
-    },
-  },
-};
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://ncov-node-api.herokuapp.com',
+//       },
+//     },
+//   },
+// };
