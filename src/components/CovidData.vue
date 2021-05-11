4be9c5354646d28f164452dcf2ff7260342181d5
Subha Chanda <subhachanda88@gmail.com>
2021-05-11 10:39:07 +0530

    Updated Covid tracker API to India

--- src/components/CovidData.vue
+++ src/components/CovidData.vue
@@ -1,45 +1,48 @@
 <template>
-    <section id="one">
-        <div class="inner">
-            <div class="row">
-                <!-- Break -->
-                <div class="col-4 col-12-medium">
-                    <h3>Total Cases in West Bengal</h3>
-                    <h2>{{data.confirmed.toLocaleString('en-IN')}}</h2>
-                </div>
-                <div class="col-4 col-12-medium">
-                    <h3>Total Recovered in West Bengal</h3>
-                    <h2>{{data.recovered.toLocaleString('en-IN')}}</h2>
-                </div>
-                <div class="col-4 col-12-medium">
-                    <h3>Total Deaths in West Bengal</h3>
-                    <h2>{{data.deaths.toLocaleString('en-IN')}}</h2>
-                </div>
-            </div>
+  <section id="one">
+    <div class="inner">
+      <div class="row">
+        <!-- Break -->
+        <div class="col-4 col-12-medium">
+          <h3>Total Cases in India</h3>
+          <h2>{{ data.cases.toLocaleString("en-IN") }}</h2>
         </div>
-    </section>
+        <div class="col-4 col-12-medium">
+          <h3>Total Recovered in India</h3>
+          <h2>{{ data.recovered.toLocaleString("en-IN") }}</h2>
+        </div>
+        <div class="col-4 col-12-medium">
+          <h3>Total Active Cases in India</h3>
+          <h2>{{ data.active.toLocaleString("en-IN") }}</h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-Vue.use(VueAxios, axios)
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 
 export default {
-    name: 'CovidData',
-    data() {
-        return {data:{
-            active: 0,
-            confirmed: 0,
-            deaths: 0
-        }}
-    },
-    mounted() {
-        Vue.axios.get('https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise').then((res) => {
-            this.data = res.data.data.statewise[7]
-            console.log(res.data.data.statewise[7]);
-        })
-    },
-}
+  name: "CovidData",
+  data() {
+    return {
+      data: {
+        cases: 0,
+        recovered: 0,
+        active: 0,
+      },
+    };
+  },
+  mounted() {
+    Vue.axios
+      .get("https://corona.lmao.ninja/v3/covid-19/countries/india?strict=true")
+      .then((res) => {
+        this.data = res.data;
+      });
+  },
+};
 </script>
